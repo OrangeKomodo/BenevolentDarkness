af42c5d14ab35f902216b26831e13a3d00382480
OrangeKomodo <101063140+OrangeKomodo@users.noreply.github.com>
2026-09-24 19:31:12 -0500

    Extreme Force Refector
    -Not much new here, just made some lines more clear
    -Also added comments

--- Assets/Scripts/Spells/ExtremeForce.cs
+++ Assets/Scripts/Spells/ExtremeForce.cs
@@ -7,15 +7,21 @@ namespace Spells
 		public BoxCollider2D BoxCollider;
 		public AreaEffector2D AreaEffector2D;
 
+		public float MaxDistance = 200f;
+		public float GapBeforeWall = 3f;
+		public LayerMask WhatIsWall;
+
 		private void Start()
 		{
 			PlayerController.PlaySound("Extreme Force");
 
-			float wallDistance = Physics2D.Raycast(transform.position, transform.right, 200f, LayerMask.GetMask("Walls"))
-				.distance - 3f;
+			// Stretches the Area Effector to fill the room in front of the Player
+			RaycastHit2D forceHit = Physics2D.Raycast(transform.position, transform.right, MaxDistance, WhatIsWall);
+			float wallDistance = forceHit.distance - GapBeforeWall;
 			BoxCollider.size = new Vector2(wallDistance, BoxCollider.size.y);
 			BoxCollider.offset = new Vector2(wallDistance / 2f, BoxCollider.offset.y);
 
+			// Set the force angle to face away from the Player
 			AreaEffector2D.forceAngle = transform.rotation.y == 1f ? 180 : 0;
 		}
 	}
